800aca6e7e624812a6435dc25ac0a88e344e0c47
André Quintino <pandrequintino@gmail.com>
2022-05-24 22:54:33 +0100

    docs (challenge05): add documentation with swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
